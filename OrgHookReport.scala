2c428c6f054967eb11391b6784c8456dc1f99e01
Roberto Tyley <roberto.tyley@guardian.co.uk>
2012-10-31 15:10:52 +0000

    Add willy-nilly setting of a pivotaltracker token

--- OrgHookReport.scala
+++ OrgHookReport.scala
@@ -2,6 +2,7 @@ import org.eclipse.egit.github.core
 import core.{RepositoryHook, Repository}
 import scala.collection.immutable.{SortedMap, TreeMap}
 import scala.collection.JavaConversions._
+import tools.jline.console.ConsoleReader
 
 object OrgHookReport extends App {
 
@@ -13,14 +14,26 @@ object OrgHookReport extends App {
     println("%s (%d hooks)".format(rd.name,rd.hooks.size))
     rd.hooks.foreach {
       hook =>
-        println("\t%s : %s".format(hook.getName, sanitisedConfigFor(hook)))
+        println("\t%s : %s %s".format(hook.getName, hook.getUrl, sanitisedConfigFor(hook)))
     }
   }
+
+  println("Enter the Pivotal Tracker token for the 'github' user...")
+
+  val cr = new ConsoleReader
+  val token = cr.readLine("token>")
+
+  val hook: RepositoryHook = new RepositoryHook()
+  hook.setActive(true)
+  hook.setName("pivotaltracker")
+  hook.setConfig(Map("token" -> token))
+
+  repositories.par.foreach { r=>  GitHub.repositoryService.createHook(r, hook) }
 
 
   def sanitisedConfigFor(hook: RepositoryHook): SortedMap[String, String] = {
     val config = hook.getConfig.map {
-      case (k, v) if Set("token", "password").contains(k) => (k, v.take(2) + "...")
+      case (k, v) if Set("token", "password").contains(k) => (k, v.take(3) + "...")
       case c => c
     }
     TreeMap(config.toList: _*)
